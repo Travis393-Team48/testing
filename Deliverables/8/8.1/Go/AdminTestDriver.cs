967f2aeefb0d6c7a710c44134f0401fce9d20c24
Calvin <43643839+bot1324@users.noreply.github.com>
2019-11-20 19:59:39 -0600

    Admin TestDriver read Default Player from config file

--- Deliverables/8/8.1/Go/AdminTestDriver.cs
+++ Deliverables/8/8.1/Go/AdminTestDriver.cs
@@ -21,10 +21,18 @@ namespace Go
             //Create remote player (will need explicitly create a remote player to connect to this)
             PlayerWrapper player1 = new PlayerWrapper(port);
 
+
+            string goPlayer = File.ReadAllText(path);
+            JObject player = JsonConvert.DeserializeObject<JObject>(goPlayer);
+
+            int depth = player["depth"].ToObject<int>();
+
             //Create local player
-            PlayerWrapper player2 = new PlayerWrapper("less dumb");
+            PlayerWrapper player2 = new PlayerWrapper("less dumb", depth);
+
 
             List<string> victors = Admin.AdministerGame(player1, "remote player", player2, "local player", 9);
+
             Console.WriteLine(JsonConvert.SerializeObject(victors.ToArray()));
 
             //Console.ReadLine();
--- Deliverables/8/8.1/RemotePlayer/Program.cs
+++ Deliverables/8/8.1/RemotePlayer/Program.cs
@@ -17,7 +17,7 @@ namespace RemotePlayer
             string go = File.ReadAllText("go.config");
             JObject config = JsonConvert.DeserializeObject<JObject>(go);
 
-            PlayerClient player = new PlayerClient(config["IP"].ToObject<string>(), config["port"].ToObject<int>(), "smart", 1, "no name");
+            PlayerClient player = new PlayerClient(config["IP"].ToObject<string>(), config["port"].ToObject<int>(), "smart", 1, "remote player");
             while (true)
             {
                 if (player.IsConnected())
